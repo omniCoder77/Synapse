4ddff1b1662fc06d148e473e86276582fd4bf77a
omniCoder77 <rishabh_2024bcse025@nitsri.ac.in>
2025-07-17 02:31:27 +0530

    Added atomicity to rate limiting with redis using lua script

--- auth-service/src/main/kotlin/com/ethyllium/authservice/application/config/RedisConfig.kt
+++ auth-service/src/main/kotlin/com/ethyllium/authservice/application/config/RedisConfig.kt
@@ -3,15 +3,16 @@ package com.ethyllium.authservice.application.config
 import com.ethyllium.authservice.domain.model.UserRegisteredEvent
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.ReactiveRedisOperations
 import org.springframework.data.redis.core.ReactiveRedisTemplate
 import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.data.redis.core.script.RedisScript
 import org.springframework.data.redis.serializer.*
 
 
@@ -67,5 +68,10 @@ class RedisConfig {
         val context = builder.value(valueSerializer).build()
 
         return ReactiveRedisTemplate(factory, context)
+    }
+
+    @Bean
+    fun script(): RedisScript<Boolean> {
+        return RedisScript.of(ClassPathResource("scripts/rateLimiter.lua"), Boolean::class.java)
     }
 }
--- auth-service/src/main/kotlin/com/ethyllium/authservice/infrastructure/adapters/outbound/security/RateLimiterHandlerFilterFunction.kt
+++ auth-service/src/main/kotlin/com/ethyllium/authservice/infrastructure/adapters/outbound/security/RateLimiterHandlerFilterFunction.kt
@@ -1,8 +1,8 @@
 package com.ethyllium.authservice.infrastructure.adapters.outbound.security
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.data.redis.core.ReactiveRedisCallback
 import org.springframework.data.redis.core.ReactiveRedisTemplate
+import org.springframework.data.redis.core.script.RedisScript
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.HandlerFilterFunction
@@ -11,8 +11,6 @@ import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Mono
 import java.net.InetSocketAddress
-import java.nio.ByteBuffer
-import java.time.Duration
 import java.time.LocalTime
 import java.util.*
 
@@ -20,32 +18,21 @@ import java.util.*
 @Component
 class RateLimiterHandlerFilterFunction(
     private val reactiveRedisTemplate: ReactiveRedisTemplate<String, Long>,
-    @Value("\${MAX_REQUESTS_PER_MINUTE}") private val MAX_REQUESTS_PER_MINUTE: Long
+    @Value("\${MAX_REQUESTS_PER_MINUTE}") private val maxRequestPerMinute: Long,
+    private val script: RedisScript<Boolean>
 ) : HandlerFilterFunction<ServerResponse, ServerResponse> {
     override fun filter(request: ServerRequest, next: HandlerFunction<ServerResponse?>): Mono<ServerResponse?> {
         val currentMinute = LocalTime.now().minute
         val key = String.format("rl_%s:%s", requestAddress(request.remoteAddress()), currentMinute)
 
         return reactiveRedisTemplate
-            .opsForValue().get(key)
-            .flatMap(
-                { value ->
-                    if (value >= MAX_REQUESTS_PER_MINUTE)
-                        ServerResponse.status(HttpStatus.TOO_MANY_REQUESTS).build() else
-                        incrAndExpireKey(key, request, next)
-                }
-            ).switchIfEmpty(incrAndExpireKey(key, request, next))
-    }
-
-    private fun incrAndExpireKey(
-        key: String, request: ServerRequest, next: HandlerFunction<ServerResponse?>
-    ): Mono<ServerResponse?> {
-        return reactiveRedisTemplate.execute(ReactiveRedisCallback<MutableList<Any?>?> { connection ->
-            val bbKey: ByteBuffer = ByteBuffer.wrap(key.toByteArray())
-            Mono.zip(
-                connection.numberCommands().incr(bbKey), connection.keyCommands().expire(bbKey, Duration.ofSeconds(59L))
-            ).then(Mono.empty())
-        }).then(next.handle(request))
+            .execute(script, listOf(key), listOf(maxRequestPerMinute, 59))
+            .single(false)
+            .flatMap({ value ->
+                if (value)
+                    ServerResponse.status(HttpStatus.TOO_MANY_REQUESTS).build() else
+                    next.handle(request)
+            })
     }
 
     private fun requestAddress(maybeAddress: Optional<InetSocketAddress>): String? {
