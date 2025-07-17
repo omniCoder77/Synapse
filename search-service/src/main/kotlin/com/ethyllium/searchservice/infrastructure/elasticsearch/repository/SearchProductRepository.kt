60335bbcf6f997eac367b0ecb846c67f8218dafc
omniCoder77 <rishabh_2024bcse025@nitsri.ac.in>
2025-07-17 23:30:48 +0530

    migrated to reactive stack

--- search-service/src/main/kotlin/com/ethyllium/searchservice/infrastructure/elasticsearch/repository/SearchProductRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.ethyllium.searchservice.infrastructure.elasticsearch.repository
-
-import com.ethyllium.searchservice.infrastructure.elasticsearch.entity.SearchProduct
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface SearchProductRepository: ElasticsearchRepository<SearchProduct, String> {
-}
